78c16e133ca2969efdb7c8834f86cb5edff466ed
narisettichaitanyanaidu <narisettichaitanyanaidu@gmail.com>
2025-04-29 08:23:35 +0000

    Commit changes

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: 'jest-expo',
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?@?react-native|@react-native-community|expo|@expo|expo-location|@unimodules|unimodules|@react-navigation|@react-native|@firebasegen)'
+  ],
+};
---
+++ src/services/__tests__/alerts.test.ts
@@ -0,0 +1,7 @@
+import * as AlertsService from '../alerts';
+
+describe('AlertsService', () => {
+  it('should be defined', () => {
+    expect(AlertsService).toBeDefined();
+  });
+});
